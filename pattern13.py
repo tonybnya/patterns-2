b033ca2aaddf8f48896bd395d7b0bd3b2481a245
Tony <nya.tony2010@gmail.com>
2023-09-02 13:22:40 +0100

    Add: pattern 13

--- pattern13.py
+++ pattern13.py
@@ -9,8 +9,39 @@
 
 
 def func(n, star='*'):
-    pass
+    space = ' '
+
+    for i in range(1, n + 1):
+        if i == 1 or i == n:
+            # spaces
+            for j in range(n - i):
+                print(space, end='')
+            # stars
+            if i == 1:
+                for j in range(i):
+                    print(star, end='')
+            else:
+                for j in range(2 * n - 1):
+                    print(star, end='')
+            # spaces
+            for j in range(n - i):
+                print(space, end='')
+        else:
+            # spaces
+            for j in range(n - i):
+                print(space, end='')
+            # stars
+            print(star, end='')
+            # spaces
+            for j in range(2 * i - 3):
+                print(space, end='')
+            # stars
+            print(star, end='')
+            # spaces
+            for j in range(n - i):
+                print(space, end='')
+        print()
 
 
-n = 5
+n = 4
 func(n)
